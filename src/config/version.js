e6f6210f0ecd768231d4e43b1cd3ba1d8d8de379
Kaj Kowalski <dev@kajkowalski.nl>
2025-04-14 17:40:56 +0200

    Bump version to 0.2.0

--- src/config/version.js
+++ src/config/version.js
@@ -5,7 +5,7 @@
 const semver = require('semver');
 
 // Current version information
-const currentVersion = '0.1.1';
+const currentVersion = '0.2.0';
 
 module.exports = {
     // Current version without v prefix
